4549b6cf06f8d69f917297e7748969d3324e7f7e
Emilio Araos <ea3dev@gmail.com>
2020-02-01 01:36:47 -0700

    fixing global variables

--- blackjack.py
+++ blackjack.py
@@ -43,24 +43,25 @@ def deal_dealer():
 
 
 def deal_player():
-    player_score = 0
+    global player_score
+    global player_ace
     card_value = deal_card(player_card_frame)[0]
     if card_value == 1 and not player_ace:
+        player_ace = True
         card_value = 11
     player_score += card_value
-    # if its a bust, check if there is an ace and substract
+    # if its a bust, check if there is an ace and subtract
     if player_score > 21 and player_ace:
         player_score -= 10
+        player_ace = False
     player_score_label.set(player_score)
     if player_score > 21:
         result_text.set("Dealer wins! You SUCK!")
 
-
+    print(locals())
 
 
 mainWindow = tkinter.Tk()
-
-
 mainWindow.title("BlackJack")
 mainWindow.geometry("640x480")
 mainWindow.configure(background='green')
