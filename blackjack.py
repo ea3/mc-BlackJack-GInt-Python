4a639bb066f32c3efcb7d2502c648ad94ed29092
Emilio Araos <ea3dev@gmail.com>
2020-02-01 01:49:40 -0700

    score_hand(hand) function

--- blackjack.py
+++ blackjack.py
@@ -37,28 +37,53 @@ def deal_card(frame):
     # now return the card's face value
     return next_card
 
+def score_hand(hand):
+    # Calculate total score of all cards in the list.
+    # Only one ace can have the value 11, and this will reduce to 1 if the hand busts.
+    score = 0
+    ace = False
+    for next_card in hand:
+        card_value = next_card[0]
+        if card_value == 1 and not ace:
+            ace = True
+            card_value = 11
+            score += card_value
+            # if we bust, check if there is an ace and subtract 10
+        if score > 21 and ace:
+            score -= 10
+            ace = False
+    return score
+
 
 def deal_dealer():
     deal_card(dealer_card_frame)
 
 
 def deal_player():
-    global player_score
-    global player_ace
-    card_value = deal_card(player_card_frame)[0]
-    if card_value == 1 and not player_ace:
-        player_ace = True
-        card_value = 11
-    player_score += card_value
-    # if its a bust, check if there is an ace and subtract
-    if player_score > 21 and player_ace:
-        player_score -= 10
-        player_ace = False
+    player_hand.append(deal_card(player_card_frame))
+    player_score = score_hand(player_hand)
+
     player_score_label.set(player_score)
     if player_score > 21:
-        result_text.set("Dealer wins! You SUCK!")
+        result_text.set("Dealer WINS! YOU SUCK!!!")
 
-    print(locals())
+
+    # global player_score
+    # global player_ace
+    # card_value = deal_card(player_card_frame)[0]
+    # if card_value == 1 and not player_ace:
+    #     player_ace = True
+    #     card_value = 11
+    # player_score += card_value
+    # # if its a bust, check if there is an ace and subtract
+    # if player_score > 21 and player_ace:
+    #     player_score -= 10
+    #     player_ace = False
+    # player_score_label.set(player_score)
+    # if player_score > 21:
+    #     result_text.set("Dealer wins! You SUCK!")
+    #
+    # print(locals())
 
 
 mainWindow = tkinter.Tk()
@@ -83,8 +108,8 @@ dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
 player_score_label = tkinter.IntVar()
-player_score = 0
-player_ace = False
+# player_score = 0
+# player_ace = False
 tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
 
