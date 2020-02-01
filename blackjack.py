66462cdf3f79473b3769ee4727d53996a6c499dd
Emilio Araos <ea3dev@gmail.com>
2020-02-01 00:32:12 -0700

    loading card images

--- blackjack.py
+++ blackjack.py
@@ -10,16 +10,16 @@ mainWindow.geometry("640x480")
 
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
-result.grid(row=0, clomn=0, columnspan=3)
+result.grid(row=0, column=0, columnspan=3)
 
 card_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="green")
-card_frame.grid(row=1, clomn=0, sticky='ew', columnspan=3, rowspan=2)
+card_frame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Dealer", background="green", fg="white").grid(row=0, column=0)
 tkinter.Label(card_frame, textvariable=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
 
-# embedded frame hold the card images
+# embedded frame to hold the card images
 
 dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
@@ -27,6 +27,20 @@ dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 player_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
+
+# player card frame
+
+player_card_frame = tkinter.Frame(card_frame, background="green")
+player_card_frame.grid(row=2, column=1, sticky="ew", rowspan=2)
+
+button_frame = tkinter.Frame(mainWindow)
+button_frame.grid(row=3, column=0, columnspan=3, sticky="w")
+
+dealer_button = tkinter.Button(button_frame, text="Dealer")
+dealer_button.grid(row=0, column=0)
+
+player_button = tkinter.Button(button_frame, text="Player")
+player_button.grid(row=0, column=1)
 
 
 
